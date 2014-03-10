9063b61dade15f5b3bb09a8d17f0993555da4564
Ben Hood <0x6e6562@gmail.com>
2014-03-10 11:08:56 +0000

    Updated example to reflect current API

--- main.go
+++ main.go
@@ -1,53 +1,56 @@
 package main
 
 import (
-    "github.com/relops/cqlc/cqlc"
-    "log"
-    "tux21b.org/v1/gocql"
-    "twitter"
+	"github.com/gocql/gocql"
+	"github.com/relops/cqlc/cqlc"
+	"log"
+	"twitter"
 )
 
 var TWEETS = twitter.TweetsTableDef()
 
 func main() {
 
-    host := "127.0.0.1"
-    keyspace := "twitter_example"
+	host := "127.0.0.1"
+	keyspace := "twitter_example"
 
-    cluster := gocql.NewCluster(host)
-    cluster.Keyspace = keyspace
-    session, err := cluster.CreateSession()
+	cluster := gocql.NewCluster(host)
+	cluster.Keyspace = keyspace
+	session, err := cluster.CreateSession()
 
-    if err != nil {
-        log.Fatalf("Could not create CQL session: %s", err)
-    }
+	if err != nil {
+		log.Fatalf("Could not create CQL session: %s", err)
+	}
 
-    ctx := cqlc.NewContext()
+	ctx := cqlc.NewContext()
 
-    err = ctx.Upsert(TWEETS).
-        SetString(TWEETS.NAME, "tweeter").
-        Where(TWEETS.ID.Eq(1)).
-        Exec(session)
+	err = ctx.Upsert(TWEETS).
+		SetString(TWEETS.NAME, "tweeter").
+		Where(TWEETS.ID.Eq(1)).
+		Exec(session)
 
-    if err != nil {
-        log.Fatalf("Could not execute CQL upsert: %s", err)
-    }
+	if err != nil {
+		log.Fatalf("Could not execute CQL upsert: %s", err)
+	}
 
-    iter, err := ctx.Select().
-        From(TWEETS).
-        Where(TWEETS.ID.Eq(1)).
-        Fetch(session)
+	iter, err := ctx.Select().
+		From(TWEETS).
+		Where(TWEETS.ID.Eq(1)).
+		Fetch(session)
 
-    if err != nil {
-        log.Fatalf("Could not execute CQL select: %s", err)
-    }
+	if err != nil {
+		log.Fatalf("Could not execute CQL select: %s", err)
+	}
 
-    tweets := twitter.BindTweets(iter)
+	tweets, err := twitter.BindTweets(iter)
+	if err != nil {
+		log.Fatalf("Could not bind tweets: %s", err)
+	}
 
-    err = iter.Close()
-    if err != nil {
-        log.Fatalf("Could not bind tweets: %s", err)
-    }
+	err = iter.Close()
+	if err != nil {
+		log.Fatalf("Could not bind tweets: %s", err)
+	}
 
-    log.Printf("Got tweets: %+v\n", tweets)
+	log.Printf("Got tweets: %+v\n", tweets)
 }
